6bd0d50efa72d126c3890d1dab26ff2edb1d6d87
Błażej Niewiadomski <blaz.nie@protonmail.com>
2025-12-25 00:06:58 +0100

    update diff faces

--- doom-gruber-darker-theme.el
+++ doom-gruber-darker-theme.el
@@ -31,7 +31,7 @@ determine the exact padding."
     "A theme based on Alexey Kutepov's Gruber Darker theme."
   ;; name        gui       256       16
   ((bg         '("#060606" nil       nil          ))
-   (bg-alt     '("#101010" nil       nil          ))
+   (bg-alt     '("#121212" nil       nil          ))
    (base0      '("#0d0d0d" "black"   "black"      ))
    (base1      '("#1b1b1b" "#1b1b1b"              ))
    (base2      '("#212122" "#1e1e1e"              ))
@@ -42,12 +42,14 @@ determine the exact padding."
    (base7      '("#969896" "#979797" "brightblack"))
    (base8      '("#ffffff" "#ffffff" "white"      ))
    (fg         '("#f5f5f5"))
-   (fg-alt     (doom-darken fg 0.4))
+   (fg-alt      (doom-darken fg 0.4))
    (grey       '("#8b8b83"))
    (red        '("#f43841" "#ee6666" "red"))
+   (light-red   (doom-lighten red 0.2))
    (orange     '("#ef532f" "#dd9955" "brightred"))
    (yellow     '("#ffdd33" "#ffdd33" "yellow"))
    (green      '("#73c936" "#73c936" "green"))
+   (light-green (doom-lighten green 0.2))
    (blue       '("#81a2be" "#88aabb" "brightblue"))
    (dark-blue  '("#303540" "#41728e" "blue"))
    (teal       '("#81a2be")) ; FIXME replace with real teal
@@ -58,7 +60,7 @@ determine the exact padding."
 
    ;; face categories
    (highlight      base7)
-   (vertical-bar   base0)
+   (vertical-bar   base4)
    (selection      `(,(car (doom-lighten bg 0.1)) ,@(cdr base3)))
    (builtin        yellow)
    (comments       grey)
@@ -100,22 +102,17 @@ determine the exact padding."
     :box (if -modeline-pad `(:line-width ,-modeline-pad :color ,modeline-bg-alt)))
 ;;;; window dividers
    (window-divider :background fg :foreground fg)
-   (internal-border :background bg :width 'narrow)
-   (border :background bg :foreground fg :width 'narrow)
+   (internal-border :background bg :foreground base4 :width 'narrow)
+   (border :background bg :foreground base4 :width 'narrow)
 ;;;; org mode
-   (org-imminent-deadline :box `(:line-width -1 :color ,orange :style nil)
-                          :foreground orange)
+   (org-imminent-deadline :box `(:line-width -1 :color ,orange :style nil) :foreground orange)
    (org-block-begin-line :foreground grey  :background bg-alt :extend t)
    (org-block-end-line :foreground grey :background bg-alt :extend t)
-   (org-document-info-keyword :box `(:line-width -1 :color ,base5 :style nil)
-                              :background bg
-                              :foreground base5)
+   (org-document-info-keyword :box `(:line-width -1 :color ,base5 :style nil) :background bg :foreground base5)
    (org-meta-line :background bg-alt :foreground comments :extend t)
    (org-todo :background bg :foreground green)
    (org-done :background bg :foreground base5)
-   (org-drawer :box `(:line-width -1 :color ,grey :style nil)
-               :background bg
-               :foreground grey)
+   (org-drawer :box `(:line-width -1 :color ,grey :style nil) :background bg :foreground grey)
    (org-transclusion :background bg-alt)
 ;;;; yasnippet
    (yas-field-highlight-face :weight regular)
@@ -141,21 +138,23 @@ determine the exact padding."
    (eldoc-box-border :width 'narrow :background base1)
 ;;;; show paren mode
    (show-paren-match :background red :foreground 'unspecified)
-   (show-paren-match-expression :box nil
-                                :background bg)
+   (show-paren-match-expression :box nil :background bg)
 ;;;; window-stool
-   (window-stool-face :background bg-alt
-                      :foreground fg-alt)
+   (window-stool-face :background bg-alt :foreground fg :extend t)
 ;;;; diff-hl
-   (diff-hl-margin-insert :background bg :foreground green)
-   (diff-hl-margin-delete :background bg :foreground red)
-   (diff-hl-margin-change :background bg :foreground base6)
+   (diff-hl-margin-insert :foreground success :foreground success)
+   (diff-hl-margin-delete :background error :foreground error)
+   (diff-hl-margin-change :background cyan :foreground cyan)
    (diff-hl-insert :background bg)
    (diff-hl-delete :background bg)
    (diff-hl-change :background bg)
-   (diff-refine-added :background bg :foreground success :underline t)
-   (diff-refine-changed :background bg :foreground blue :underline t)
-   (diff-refine-removed :background bg :foreground error :underline t)
+;;;; diffs
+   (diff-added :background (doom-darken green 0.5) :foreground green)
+   (diff-removed :background (doom-darken red 0.5) :foreground red)
+   (diff-changed :background (doom-darken cyan 0.5) :foreground cyan)
+   (diff-refine-added :background (doom-darken success 0.5) :foreground (doom-darken success 0.3) :underline t)
+   (diff-refine-changed :background (doom-darken cyan 0.5) :foreground (doom-darken blue 0.3)  :underline t)
+   (diff-refine-removed :background (doom-darken error 0.5) :foreground (doom-darken error 0.3) :underline t)
 ;;;; corfu
    (corfu-border :background base5 :foreground fg-alt)
    (corfu-current :background (doom-lighten bg-alt 0.2) :foreground fg)
@@ -164,21 +163,18 @@ determine the exact padding."
    (corfu-echo :background bg :foreground fg)
 ;;;; lsp
    (lsp-lens-face :height 1)
-   (lsp-signature-highlight-function-argument :background fg-alt
-                                              :foreground bg
-                                              :slant 'italic)
+   (lsp-signature-highlight-function-argument :background fg-alt :foreground bg :slant 'italic)
    (lsp-ui-peek-header :background bg :foreground fg-alt)
    (lsp-ui-peek-footer :background bg :foreground fg-alt)
    (lsp-ui-peek-peek :background (doom-lighten bg 0.1) :foreground fg-alt)
    (lsp-ui-peek-list :background (doom-lighten bg 0.1) :foreground fg-alt)
    (lsp-ui-doc-background :background bg-alt)
 ;;;; company
-   (company-tooltip-common :weight 'semi-bold
-                           :inherit 'company-tooltip)
+   (company-tooltip-common :weight 'semi-bold :inherit 'company-tooltip)
    (company-tooltip :foreground fg :background base0)
    (company-tooltip-selection :background base1)
 ;;;; doom-modeline
-   (doom-modeline-buffer-path       :foreground violet :bold bold)
+   (doom-modeline-buffer-path :foreground violet :bold bold)
    (doom-modeline-buffer-major-mode :inherit 'doom-modeline-buffer-path)
 ;;;; child frame
    (child-frame-border :foreground fg :background fg)
@@ -188,22 +184,14 @@ determine the exact padding."
    (font-lock-comment-face :slant 'italic :foreground comments)
    (font-lock-doc-face :slant 'italic :foreground comments)
 ;;;; line numbers
-   (line-number-current-line :foreground yellow :bold bold
-                             :inherit '(hl-line default))
+   (line-number-current-line :foreground yellow :bold bold :inherit '(hl-line default))
    (whitespace-space :foreground fg-alt)
-   (whitespace-hspace :foreground bg
-                      :background bg)
-   (whitespace-indentation :foreground base4
-                           :background base4)
-   (menu :background base0
-         :box nil
-         :foreground fg)
-   (tool-bar :background bg
-             :foreground fg
-             :box nil)
+   (whitespace-hspace :foreground bg :background bg)
+   (whitespace-indentation :foreground base4 :background base4)
+   (menu :background base0 :box nil :foreground fg)
+   (tool-bar :background bg :foreground fg :box nil)
 ;;;; popup
-   (popup-tip-face :background bg-alt
-                   :foreground fg)
+   (popup-tip-face :background bg-alt :foreground fg)
 ;;;; tty menu
    (tty-menu-enabled-face :background bg-alt :foreground fg :weight 'bold)
    (tty-menu-disabled-face :background selection :foreground fg :weight 'normal)
@@ -216,47 +204,33 @@ determine the exact padding."
    (flymake-error-echo-at-eol :foreground error :background bg-alt :weight 'regular)
 ;;;; sideline
    (sideline-lsp-code-action :foreground base6)
-   (sideline-blame :slant 'normal
-                   :extend t
-                   :height 1.0
-                   :background bg-alt
-                   :foreground base6)
+   (sideline-blame :slant 'normal :extend t :height 1.0 :background bg-alt :foreground base6)
 ;;;; treemacs
-   (treemacs-root-face :background bg-alt
-                       :foreground blue)
+   (treemacs-root-face :background bg-alt :foreground blue)
 ;;;; avy
    (avy-lead-face :background violet)
 ;;;; anzu
-   (anzu-mode-line :foreground fg
-                   :weight 'bold)
+   (anzu-mode-line :foreground fg :weight 'bold)
 ;;;; meow
-   (meow-insert-indicator :background yellow
-                          :foreground bg-alt
-                          :extend t)
-   (meow-beacon-indicator :background blue
-                          :foreground bg-alt)
-   (meow-motion-indicator :background cyan
-                          :foreground fg)
-   (meow-normal-indicator :background warning
-                          :foreground bg-alt)
-   (meow-keypad-indicator :background violet
-                          :foreground bg-alt)
-   (meow-search-indicator :background bg-alt
-                          :foreground base6)
+   (meow-insert-indicator :background yellow :foreground bg-alt :extend t)
+   (meow-beacon-indicator :background blue :foreground bg-alt)
+   (meow-motion-indicator :background cyan :foreground fg)
+   (meow-normal-indicator :background warning :foreground bg-alt)
+   (meow-keypad-indicator :background violet :foreground bg-alt)
+   (meow-search-indicator :background bg-alt :foreground base6)
 
    (meow-search-highlight :inherit 'region)
    (meow-beacon-fake-selection :inherit 'region)
-   (meow-beacon-fake-cursor :background (doom-lighten selection 0.3)
-                            :foreground base0)
+   (meow-beacon-fake-cursor :background (doom-lighten selection 0.3) :foreground base0)
    (region :background (doom-lighten bg 0.1) :extend nil)
 
-   (secondary-selection :background (doom-lighten bg 0.05)
-                        :foreground 'unspecified
-                        :extend nil)
-
+   (secondary-selection :background (doom-lighten bg 0.05) :foreground 'unspecified :extend nil)
+;;;;
+   (mouse :background fg)
 ;;;; devdocs
-   (devdocs-code-block :background base4
-                       :extend t)
+   (devdocs-code-block :background base4 :extend t)
+
+   (scroll-bar :foreground bg :background base4)
    ;; --- variables --------------------------
    ;; ()
    ))
